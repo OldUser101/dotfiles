8f8de338d4bacd16a42c9fd71d9c9d71eae5812a
Nathan Gill <nathan.j.gill@outlook.com>
2025-12-20 17:08:53 +0000

    feat: add dunst configuration

--- olduser101.nix
+++ olduser101.nix
@@ -4,6 +4,7 @@
   # OldUser101 configuration root, just imports application specific configs
 
   imports = [
+    ./dunst/dunst.nix
     ./kitty/kitty.nix
     ./sway/sway.nix
     ./oh-my-zsh/oh-my-zsh.nix
