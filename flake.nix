a131c6a99865bd10520c855467522029ce165bf8
Nathan Gill <nathan.j.gill@outlook.com>
2026-03-30 00:16:48 +0100

    feat: remove `nixpkgs` pin from `nlock` for caching

--- flake.nix
+++ flake.nix
@@ -14,10 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nlock = {
-      url = "github:OldUser101/nlock/v0.1.2";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nlock.url = "github:OldUser101/nlock/v0.1.2";
 
     lic = {
       url = "git+https://tangled.org/nathanjgill.uk/lic";
