9a6c2aab56acf267c0774ad5eb1ab9e435e6e3af
Nathan Gill <nathan.j.gill@outlook.com>
2026-04-23 20:49:28 +0100

    feat(natha/fonts): add CJK and emoji fonts

--- modules/home/natha/fonts/default.nix
+++ modules/home/natha/fonts/default.nix
@@ -31,6 +31,9 @@ in
       with pkgs;
       [
         noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-color-emoji
         nerd-fonts.fira-code
         fira-code
         fira-code-symbols
