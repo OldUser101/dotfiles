21bcff76ea32892ed572af189b791d48ed5eb5ff
Nathan Gill <n@ngill.net>
2026-05-28 17:45:15 +0100

    chore: bump `nlock` to version 0.1.5

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nlock.url = "github:OldUser101/nlock/v0.1.4";
+    nlock.url = "github:OldUser101/nlock/v0.1.5";
 
     lic = {
       url = "github:OldUser101/lic";
